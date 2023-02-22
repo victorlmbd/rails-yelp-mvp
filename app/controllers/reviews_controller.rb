f369eb46da3fd217e7e1a5dd0db2dd721b30120f
victor lombard <victor@lombard.fr>
2023-02-22 19:04:45 +0100

    exercice2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,18 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
